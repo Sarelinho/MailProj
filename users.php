155df8fea671de7bd79caf1e7bc4b716e04ff3ce
Sarelinho <101663982+Sarelinho@users.noreply.github.com>
2023-09-19 14:24:36 +0300

    Update users.php

--- users.php
+++ users.php
@@ -8,20 +8,8 @@ class users
         $this->mysql=$conn;
     }
 
-    private function EncPass($p){
         $p = "AAA";
-        return md5("W".$p."23"); // salt protection
-    }
-    public function IsValid($u,$p){
-        $enc_pass = $this->EncPass($p);
-        $q  = "SELECT * FROM `mailusers` ";
-        $q .= " WHERE pass='$enc_pass' ";
-        $result = mysqli_query($this->mysql, $q);
-
-        if(mysqli_num_rows($result)>0)
-            return true;
-        return false;
-    }
+    
     public function CreateUser($params){
         $id=isset($params['id']) ? $params['id'] : "";
         $name=isset($params['name'])   ? $params['name']   : "";
